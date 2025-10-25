3bbe2beabbef3cb8b3f75dd1fb4b08db8439e028
TasleemHAS <153951478+TasleemHAS@users.noreply.github.com>
2025-10-25 16:18:06 +0530

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-
-# Start User Service in background
-python user_service/app.py &
-USER_PID=$!
-
-# Start Order Service in background  
-python order_service/app.py &
-ORDER_PID=$!
-
-# Wait a moment for services to start
-sleep 5
-
-# Start Streamlit app (this will run in foreground)
+# Start User Service
+python user_service.py &
+# Start Order Service  
+python order_service.py &
+# Start Streamlit
 streamlit run streamlit_app.py --server.port=8501 --server.address=0.0.0.0
-
-# If streamlit exits, kill the background services
-kill $USER_PID
-kill $ORDER_PID
